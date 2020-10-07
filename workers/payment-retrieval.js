2009625fa7c9b3632a6b1cf8de4c0229771cede8
Maxim Monin <maximmonin@gmail.com>
2020-10-07 13:49:06 +0300

    nodejs worker paramitrization

--- workers/payment-retrieval.js
+++ workers/payment-retrieval.js
@@ -6,15 +6,22 @@ const faker = require ('faker');
 //  - 'baseUrl': url to the Process Engine
 //  - 'logger': utility to automatically log important events
 //  - 'asyncResponseTimeout': long polling timeout (then a new request will be issued)
-var url = process.env.CamundaURL || 'http://camunda:8080/engine-rest';
-var timeout = process.env.ResponseTimeout || 10000;
-const config = { baseUrl: url, use: logger, asyncResponseTimeout: timeout };
+
+//  - 'maxTasks': The maximum number of tasks to fetch from Camunda in batch
+//  - 'maxParallelExecutions': The maximum number of tasks to be worked on simultaneously
+//  Number of async execution threads = maxTasks if maxParallelExcecutions is not set
+//  Else Number of async threads = min (MaxTasks,maxParallelExcecutions)
+
+const url = process.env.CamundaURL || 'http://camunda:8080/engine-rest';
+const bpm = 'payment-retrieval';
+const timeout = process.env.ResponseTimeout || 10000;
+const config = { baseUrl: url, use: logger, asyncResponseTimeout: timeout, maxTasks: 50, maxParallelExecutions: 10 };
 
 // create a Client instance with custom configuration
 const client = new Client(config);
 
 // susbscribe to the topic: 'charge-card'
-client.subscribe('charge-card', async function({ task, taskService }) {
+client.subscribe('charge-card', {processDefinitionKey: bpm}, async function({ task, taskService }) {
   // Put your business logic here
 
   // Get a process variable
@@ -38,7 +45,7 @@ client.subscribe('charge-card', async function({ task, taskService }) {
 });
 
 // susbscribe to the topic: 'charge-card-premium'
-client.subscribe('charge-card-premium', async function({ task, taskService }) {
+client.subscribe('charge-card-premium', {processDefinitionKey: bpm}, async function({ task, taskService }) {
   // Put your business logic here
 
   // Get a process variable
@@ -53,7 +60,7 @@ client.subscribe('charge-card-premium', async function({ task, taskService }) {
 
 
 // susbscribe to the topic: 'generate-item-amount'
-client.subscribe('generate-item-amount', async function({ task, taskService }) {
+client.subscribe('generate-item-amount', {processDefinitionKey: bpm},  async function({ task, taskService }) {
 
   console.log(`Generating amount and item for process...`);
 
